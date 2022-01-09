97846541f57f3879895dc49f67c046c5fa81c473
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-09 22:01:35 +0100

    repeat string

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-function confirmEnding(str: string, target: string) {
-  console.log(str.substring(str.length - target.length));
-
-  return str.substring(str.length - target.length) === target;
+function repeatString(str: string, num: number): Array<string> | string {
+  if (num <= 0) {
+    return "";
+  }
+  return Array(num).fill(str).join("");
 }
 
-const str: string = "He is cool";
-const res = confirmEnding(str, "cool");
+function repeatString2(str: string, num: number): Array<string> | string {
+  if (num <= 0) {
+    return "";
+  }
+  return str.repeat(num);
+}
+
+const str: string = "Hello";
+const num: number = 3;
+const res = repeatString(str, num);
 console.log(res);
