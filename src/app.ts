56c29287eca6a4522dbae550e19f285d09dba75b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-09 15:24:46 +0100

    largest numbers of arrays

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
-function titleCase(str: string): string {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map((e) => e[0].toUpperCase() + e.slice(1))
-    .join(" ");
+function largestOfFour(arrays: Array<Array<number>>): number[] {
+  let maxes: number[] = [];
+  for (let i = 0; i < arrays.length; i++) {
+    const arr = arrays[i];
+    let max: number = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > max) {
+        max = arr[j];
+      }
+    }
+    maxes.push(max);
+  }
+  return maxes;
 }
 
-const str: string = "hello my guys how are YOu ?";
-const res = titleCase(str);
+const arr = [
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+];
+const res = largestOfFour(arr);
 console.log(res);
