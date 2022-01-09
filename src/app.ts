1f1c87af9e3045f337162508962ee599f91706c4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-09 15:50:35 +0100

    second degree equation

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
-function largestOfFour(arrays: Array<Array<number>>): number[] {
-  let maxes: number[] = [];
-  for (let i = 0; i < arrays.length; i++) {
-    const arr = arrays[i].sort((a: number, b: number) => b - a);
-    maxes.push(arr[0]);
+function secondDegreeSol(
+  equation: string
+): [number, number] | number | [string, string] {
+  const parts = equation.split(" ");
+  const a: number = parts[0].split("x")[0] === "" ? 1 : +parts[0].split("x")[0];
+  const b: number = parts[2].split("x")[0] === "" ? 1 : +parts[2].split("x")[0];
+
+  const c: number = parseInt(parts[4]) - parseInt(parts[6]);
+  const delta: number = b * b - 4 * a * c;
+  if (delta > 0) {
+    const sol1 = (-b - Math.sqrt(delta)) / (2 * a);
+    const sol2 = (-b + Math.sqrt(delta)) / (2 * a);
+    return [sol1, sol2];
+  } else if (delta === 0) {
+    return (-b / 2) * a;
+  } else {
+    const sol1 = `${-b / (2 * a)} - ${Math.sqrt(delta * -1) / (2 * a)}i`;
+    const sol2 = `${-b / (2 * a)} + ${Math.sqrt(delta * -1) / (2 * a)}i`;
+    return [sol1, sol2];
   }
-  return maxes;
 }
 
-const arr = [
-  [4, 5, 1, 3],
-  [13, 27, 18, 26],
-  [32, 35, 37, 39],
-  [1000, 1001, 857, 1],
-];
-const res = largestOfFour(arr);
+const equation: string = "2x^2 + x + 1 = 3";
+const res = secondDegreeSol(equation);
 console.log(res);
