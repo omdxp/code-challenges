663bda1b077bd1ae7cfdbcec0767f74fbf14d380
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-09 21:05:25 +0100

    confirm ending of string

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,9 @@
-function secondDegreeSol(
-  equation: string
-): [number, number] | number | [string, string] {
-  const parts = equation.split(" ");
-  const a: number = parts[0].split("x")[0] === "" ? 1 : +parts[0].split("x")[0];
-  const b: number = parts[2].split("x")[0] === "" ? 1 : +parts[2].split("x")[0];
+function confirmEnding(str: string, target: string) {
+  console.log(str.substring(str.length - target.length));
 
-  const c: number = parseInt(parts[4]) - parseInt(parts[6]);
-  const delta: number = b * b - 4 * a * c;
-  if (delta > 0) {
-    const sol1 = (-b - Math.sqrt(delta)) / (2 * a);
-    const sol2 = (-b + Math.sqrt(delta)) / (2 * a);
-    return [sol1, sol2];
-  } else if (delta === 0) {
-    return (-b / 2) * a;
-  } else {
-    const sol1 = `${-b / (2 * a)} - ${Math.sqrt(delta * -1) / (2 * a)}i`;
-    const sol2 = `${-b / (2 * a)} + ${Math.sqrt(delta * -1) / (2 * a)}i`;
-    return [sol1, sol2];
-  }
+  return str.substring(str.length - target.length) === target;
 }
 
-const equation: string = "2x^2 + x + 1 = 3";
-const res = secondDegreeSol(equation);
+const str: string = "He is cool";
+const res = confirmEnding(str, "cool");
 console.log(res);
