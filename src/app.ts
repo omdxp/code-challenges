73c41c87bbbcded4832792c1da34e614fd93775c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-10 09:55:30 +0100

    add to sorted array

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-function destroyer(arr: number[], ...args: number[]) {
-  return arr.filter((x) => !args.includes(x));
+function addToSortedArray(arr: number[], num: number) {
+  arr.push(num);
+  return arr.sort((a, b) => a - b);
 }
 
-const arr = [1, 2, 3, 1, 2, 3];
-const res = destroyer(arr, 2, 3);
+const arr = [1, 2, 3, 4, 5];
+const res = addToSortedArray(arr, 2.5);
 console.log(res);
