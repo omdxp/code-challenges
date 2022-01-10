78be1487b7057b245e159168470d7620351170e2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-10 10:26:23 +0100

    convert number to roman

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
-function diffArray(arr1: number[], arr2: number[]): number[] {
-  let uniques: number[] = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (!arr2.includes(arr1[i])) {
-      uniques.push(arr1[i]);
+function convertToRoman(num: number) {
+  const roman: Record<string, number> = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let result = "";
+
+  for (const key in roman) {
+    while (num >= roman[key]) {
+      result += key;
+      num -= roman[key];
     }
   }
-  for (let i = 0; i < arr2.length; i++) {
-    if (!arr1.includes(arr2[i])) {
-      uniques.push(arr2[i]);
-    }
-  }
-  return uniques;
+
+  return result;
 }
 
-const arr1 = [1, 2, 3, 5];
-const arr2 = [1, 2, 3, 4, 5];
-const res = diffArray(arr1, arr2);
-console.log(res);
+const result = convertToRoman(42);
+console.log(result);
