53a06c0e9e301a61de42a4aa4394e0e37e4a304a
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-10 10:07:58 +0100

    sum elements in a range

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,8 @@
-function rot13(str: string) {
-  let solved = "";
-  for (let i = 0; i < str.length; i++) {
-    let code = str.charCodeAt(i);
-    if (code >= 65 && code <= 77) {
-      solved += String.fromCharCode(code + 13);
-    } else if (code >= 78 && code <= 90) {
-      solved += String.fromCharCode(code - 13);
-    } else {
-      solved += String.fromCharCode(code);
-    }
+function sumInRange(arr: number[], start: number, end: number): number {
+  if (start > end) {
+    return 0;
   }
-  return solved;
+  return arr[start] + sumInRange(arr, start + 1, end);
 }
 
-const str = "SERR PBQR PNZC";
-const res = rot13(str);
-console.log(res);
+console.log(sumInRange([1, 2, 3, 4, 5], 0, 2));
