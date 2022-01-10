30be87af1838314bfa0088e2e227449ed1479ec0
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-10 18:15:21 +0100

    add two numbers

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,37 @@
-function twoSums(nums: number[], target: number): number[] {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (i !== j && nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
-  return [];
 }
 
-let nums = [2, 7, 11, 15];
-let target = 9;
-let result = twoSums(nums, target);
-console.log(result);
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): number[] {
+  let result: ListNode | null = null;
+  let current: ListNode | null = null;
+  let carry: number = 0;
+  while (l1 || l2) {
+    const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+    const node = new ListNode(sum % 10);
+    if (result) {
+      current.next = node;
+      current = current.next;
+    } else {
+      result = node;
+      current = result;
+    }
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+  if (carry > 0) {
+    current.next = new ListNode(carry);
+  }
+  return [result?.val, result?.next?.val, result?.next?.next?.val];
+}
+
+const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+const res = addTwoNumbers(l1, l2);
+console.log(res);
