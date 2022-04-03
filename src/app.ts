08105b6de7a5ee396a96b00e6ca68571ffd107cd
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-03 17:43:24 +0100

    are these strings valid anagrams ?

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
-function secondLargest(array: number[]) {
-  if (array.length < 2) return null;
-  const set = new Set(array);
-  let b: number[] = [];
-  for (const iterator of set.values()) {
-    b.push(iterator);
+function validAnagrams(s1: string, s2: string) {
+  if (s1.length !== s2.length) return false;
+  let freq1: Record<string, number> = {}; // O(n) space complexity
+  let freq2: Record<string, number> = {}; // O(n) space complexity
+  // O(n)
+  for (let i = 0; i < s1.length; i++) {
+    if (freq1.hasOwnProperty(s1[i])) freq1[s1[i]]++;
+    else freq1[s1[i]] = 1;
   }
-  b.sort((a, b) => a - b);
-  return b[b.length - 2];
+  // O(n)
+  for (let i = 0; i < s2.length; i++) {
+    if (freq2.hasOwnProperty(s2[i])) freq2[s2[i]]++;
+    else freq2[s2[i]] = 1;
+  }
+  // O(n)
+  for (const key in freq1) {
+    if (!freq2.hasOwnProperty(key) || freq2[key] !== freq1[key]) return false;
+  }
+  return true;
 }
 
-const arr = [9, 42, 53, 2, 6, 9];
-console.log(secondLargest(arr));
+console.log(validAnagrams("garden", "danger"));
