b71e82cf9ebbed2fcde421dc03012e9e32fce3ba
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-03 18:15:56 +0100

    find kth largest element

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,18 @@
-function findStart(arr: number[], target: number) {
-  if (arr[0] === target) return 0;
-  let left = 0;
-  let right = arr.length - 1;
-  let mid: number;
-  while (left < right) {
-    mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target && arr[mid - 1] < target) return mid;
-    else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+// T(n,k) = O(nlogn)
+// S(n,k) = O(1)
+function findKthLargestElement(arr: number[], k: number) {
+  if (k < 1 || k > arr.length - 1) return null;
+  const set = new Set(arr);
+  let b: number[] = [];
+  for (const iterator of set.values()) {
+    b.push(iterator);
   }
-  return -1;
+  // O(nlogn)
+  b.sort((a, b) => a - b);
+  // O(1)
+  return b[b.length - k];
 }
 
-// O(n)
-function firstAndLastPosition(arr: number[], target: number) {
-  if (!arr.includes(target)) return [-1, -1];
-  let start = findStart(arr, target);
-  let end = start;
-  while (end + 1 < arr.length && arr[end + 1] === target) {
-    end++;
-  }
-  return [start, end];
-}
-
-const arr = [2, 4, 5, 5, 5, 5, 5, 5, 7, 9, 9];
-const target = 5;
-console.log(firstAndLastPosition(arr, target));
+const arr = [332, 23, 5, 23, 6, 757];
+const k = 6;
+console.log(findKthLargestElement(arr, k));
