2b3f2c34f423bcf9b3a799f7037bbb1adf030b1c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-03 18:04:18 +0100

    first and last position of a given target in an array (binary search)

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,29 @@
-function firstAndLastPosition(arr: number[], target: number) {
-  if (!arr.includes(target)) return [-1, -1];
-  let counter = {
-    start: 0,
-    end: 0,
-  };
-  // O(n)
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      if (counter.start === 0) {
-        counter = {
-          start: i,
-          end: i,
-        };
-      } else {
-        counter.end++;
-      }
+function findStart(arr: number[], target: number) {
+  if (arr[0] === target) return 0;
+  let left = 0;
+  let right = arr.length - 1;
+  let mid: number;
+  while (left < right) {
+    mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target && arr[mid - 1] < target) return mid;
+    else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
-  return [counter.start, counter.end];
+  return -1;
+}
+
+// O(n)
+function firstAndLastPosition(arr: number[], target: number) {
+  if (!arr.includes(target)) return [-1, -1];
+  let start = findStart(arr, target);
+  let end = start;
+  while (end + 1 < arr.length && arr[end + 1] === target) {
+    end++;
+  }
+  return [start, end];
 }
 
 const arr = [2, 4, 5, 5, 5, 5, 5, 5, 7, 9, 9];
