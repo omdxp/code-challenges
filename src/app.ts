26d458057b6f7d25f260cf77ac400cd9b2f57e8c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-03 17:55:12 +0100

    first and last position of a given target in an array

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
-function validAnagrams(s1: string, s2: string) {
-  if (s1.length !== s2.length) return false;
-  let freq1: Record<string, number> = {}; // O(n) space complexity
-  let freq2: Record<string, number> = {}; // O(n) space complexity
+function firstAndLastPosition(arr: number[], target: number) {
+  if (!arr.includes(target)) return [-1, -1];
+  let counter = {
+    start: 0,
+    end: 0,
+  };
   // O(n)
-  for (let i = 0; i < s1.length; i++) {
-    if (freq1.hasOwnProperty(s1[i])) freq1[s1[i]]++;
-    else freq1[s1[i]] = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      if (counter.start === 0) {
+        counter = {
+          start: i,
+          end: i,
+        };
+      } else {
+        counter.end++;
+      }
+    }
   }
-  // O(n)
-  for (let i = 0; i < s2.length; i++) {
-    if (freq2.hasOwnProperty(s2[i])) freq2[s2[i]]++;
-    else freq2[s2[i]] = 1;
-  }
-  // O(n)
-  for (const key in freq1) {
-    if (!freq2.hasOwnProperty(key) || freq2[key] !== freq1[key]) return false;
-  }
-  return true;
+  return [counter.start, counter.end];
 }
 
-console.log(validAnagrams("garden", "danger"));
+const arr = [2, 4, 5, 5, 5, 5, 5, 5, 7, 9, 9];
+const target = 5;
+console.log(firstAndLastPosition(arr, target));
