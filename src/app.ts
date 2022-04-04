4c1bb6fed4f2c6cc25d2bced68e86fad1a4798df
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-04 10:42:24 +0100

    first non recurring character

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 // T(n) = O(n)
-// S(n) = S(n)
-function firstRecChar(str: string) {
+// S(n) = O(n)
+function firstNonRecChar(str: string) {
   let freq: Record<string, number> = {};
   for (let i = 0; i < str.length; i++) {
     if (freq.hasOwnProperty(str[i])) freq[str[i]]++;
     else freq[str[i]] = 1;
   }
   for (const key in freq) {
-    if (freq[key] > 1) return key;
+    if (freq[key] === 1) return key;
   }
   return null;
 }
 
-console.log(firstRecChar("ABAC"));
+console.log(firstNonRecChar("AABCBD"));
