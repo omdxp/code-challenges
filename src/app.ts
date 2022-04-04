2020379be8e8ad556e839043fbe62e1b795e5a25
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-04 06:20:32 +0100

    how many ways to decode this message?

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,48 @@
-// T(n,k) = O(nlogn)
-// S(n,k) = O(1)
-function findKthLargestElement(arr: number[], k: number) {
-  if (k < 1 || k > arr.length - 1) return null;
-  const set = new Set(arr);
-  let b: number[] = [];
-  for (const iterator of set.values()) {
-    b.push(iterator);
+const alph: Record<string, number> = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+};
+
+function helper(data: string, k: number, memo: number[]) {
+  if (k === 0) return 1;
+  let s = data.length - 1;
+  if (data[s] === "0") return 1;
+  if (memo[k]) return memo[k];
+  let result: number = helper(data, k - 1, memo);
+  if (k >= 2 && Number(data.slice(s, s + 2)) <= 26) {
+    result += helper(data, k - 2, memo);
   }
-  // O(nlogn)
-  b.sort((a, b) => a - b);
-  // O(1)
-  return b[b.length - k];
+  memo[k] = result;
+  return result;
 }
 
-const arr = [332, 23, 5, 23, 6, 757];
-const k = 6;
-console.log(findKthLargestElement(arr, k));
+function numWays(data: string) {
+  const memo: number[] = Array.from({ length: data.length + 1 });
+  return helper(data, data.length, memo);
+}
+
+console.log(numWays("21322144112441232442553958433"));
