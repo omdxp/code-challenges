61c5e41c4a55a945bbd4d1199283974f6e889984
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-04 18:40:52 +0100

    reverese singly linked list

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,99 @@
-// T(n) = O(logn)
-function binarySearch(arr: boolean[]) {
-  let lower = 0;
-  let upper = arr.length - 1;
-
-  while (lower <= upper) {
-    const mid = lower + Math.floor((upper - lower) / 2);
-
-    if (arr[mid - 1] === true && arr[mid] === false) return mid;
-
-    if (arr[mid - 1] === false && arr[mid] === false) upper = mid - 1;
-    else lower = mid + 1;
+class SinglyNode {
+  private _next: SinglyNode | undefined;
+  public get next(): SinglyNode | undefined {
+    return this._next;
+  }
+  public set next(v: SinglyNode | undefined) {
+    this._next = v;
   }
 
-  return -1;
-}
-
-function percentage(arr: boolean[], index: number) {
-  return (index / arr.length) * 100;
-}
-
-function longestBuildRun(buildRuns: boolean[][]) {
-  let stack = [];
-  for (let i = 0; i < buildRuns.length; i++) {
-    stack.push(percentage(buildRuns[i], binarySearch(buildRuns[i])));
+  constructor(public value: number) {
+    this.value = value;
+    this.next = undefined;
   }
-  return stack.indexOf(Math.max(...stack));
 }
 
-const buildRuns = [
-  [true, true, true, true, false, false, false],
-  [true, true, false, false],
-  [true, true, true, false],
-  [true, true, true, true, false],
-  [true, true, true, false],
-];
+class SinglyLinkedList {
+  private _head: SinglyNode | undefined;
+  public get head(): SinglyNode | undefined {
+    return this._head;
+  }
+  public set head(v: SinglyNode | undefined) {
+    this._head = v;
+  }
 
-console.log(longestBuildRun(buildRuns));
+  private _tail: SinglyNode | undefined;
+  public get tail(): SinglyNode | undefined {
+    return this._tail;
+  }
+  public set tail(v: SinglyNode | undefined) {
+    this._tail = v;
+  }
+
+  private _size: number;
+  public get size(): number {
+    return this._size;
+  }
+
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+    this._size = 0;
+  }
+
+  push(value: number) {
+    const newNode = new SinglyNode(value);
+    if (this.size === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+    this._size++;
+    return this;
+  }
+
+  print() {
+    let str = "";
+    let curr = this.head;
+    while (curr) {
+      str += curr.value + " -> ";
+      curr = curr.next;
+    }
+    str += "null";
+    console.log(str);
+  }
+}
+
+function reverseLinkedList(head: SinglyNode | undefined) {
+  let prev: SinglyNode | undefined;
+  let curr = head;
+  let next: SinglyNode | undefined;
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
+
+function print(head: SinglyNode | undefined) {
+  let str = "";
+  let curr = head;
+  while (curr) {
+    str += curr.value + " -> ";
+    curr = curr.next;
+  }
+  str += "null";
+  console.log(str);
+}
+
+const l = new SinglyLinkedList();
+
+l.push(1).push(2).push(3).push(4);
+
+l.print();
+
+print(reverseLinkedList(l.head));
