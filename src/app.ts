2db1c5a58d27a6d0937f40ba81635d9bcb4c4c15
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-05 10:37:40 +0100

    map input to output

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,58 @@
-let pos = 2;
-let len = 10;
+const keyboardMarix = [
+  ["A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["Q", "S", "D", "F", "G", "H", "J", "K", "L", "M"],
+  ["W", "X", "C", "V", "B", "N"],
+];
 
-function step() {
-  if (pos === 0) {
-    pos++;
-  } else if (pos === len) {
-    pos--;
-  } else {
-    if (Math.random() > 0.5) {
-      pos++;
-    } else {
-      pos--;
+interface XRange {
+  x0: number;
+  x1: number;
+}
+
+interface YRange {
+  y0: number;
+  y1: number;
+}
+
+type Keyboard = Record<string, string>;
+
+function getKeyboard() {
+  let keyboard: Partial<Keyboard> = {};
+  for (let i = 0; i < keyboardMarix.length; i++) {
+    for (let j = 0; j < keyboardMarix[i].length; j++) {
+      const xRange: XRange = {
+        x0: j,
+        x1: j + 1,
+      };
+      const yRange: YRange = {
+        y0: i,
+        y1: i + 1,
+      };
+      keyboard[`${xRange.x0}:${xRange.x1}|${yRange.y0}:${yRange.y1}`] =
+        keyboardMarix[i][j];
     }
   }
+  return keyboard;
 }
 
-let found = false;
-
-for (let i = 0; i < len; i++) {
-  let attempt = i;
-  console.log("Gonna check", attempt);
-  if (attempt === pos) {
-    found = true;
-    console.log("Found the rabbit at", pos);
-    break;
-  }
-  step();
-}
-
-if (!found) {
-  for (let i = 1; i < len; i++) {
-    let attempt = i;
-    console.log("Gonna check", attempt);
-    if (attempt === pos) {
-      found = true;
-      console.log("Found the rabbit at", pos);
-      break;
+function mapInputToOutput(x: number, y: number) {
+  let keyboard = getKeyboard();
+  for (const key in keyboard) {
+    const xSplit = key.split("|")[0];
+    const ySplit = key.split("|")[1];
+    const xRange: XRange = {
+      x0: Number(xSplit.split(":")[0]),
+      x1: Number(xSplit.split(":")[1]),
+    };
+    const yRange: YRange = {
+      y0: Number(ySplit.split(":")[0]),
+      y1: Number(ySplit.split(":")[1]),
+    };
+    if (x >= xRange.x0 && x <= xRange.x1 && y >= yRange.y0 && y <= yRange.y1) {
+      return keyboard[key];
     }
-    step();
   }
+  return null;
 }
 
-if (!found) {
-  console.log("Tough luck :-(");
-}
+console.log(mapInputToOutput(5, 2.8));
